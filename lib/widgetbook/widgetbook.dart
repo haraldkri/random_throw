6216320fa13354deae4b390333cd2414033a53f6
Harald Kriebisch <kriebisch.harald+github@gmail.com>
2024-10-14 21:53:16 +0200

    Add(widgetbook)

---
+++ lib/widgetbook/widgetbook.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:random_throw/widgetbook/widgetbook.directories.g.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+/// dart run build_runner build -d
+void main() {
+  runApp(const WidgetbookApp());
+}
+
+@widgetbook.App()
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      // The [directories] variable does not exist yet,
+      // it will be generated in the next step
+      directories: directories,
+    );
+  }
+}
